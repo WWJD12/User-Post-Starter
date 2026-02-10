a1f3a4f321958c33adbff11403aa7f0701f94b96
curt vance <c.vance7777@hotmail.com>
2026-02-10 14:43:53 -0500

    update

--- user.js
+++ user.js
@@ -3,29 +3,16 @@ const event = localStorage.getItem("id")
 
 async function onSearchChange(event) {
   const id = event.target.value
-  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-  const postsData = await posts.json()
-console.log(postsData)
-postListEl.innerHTML = postsData.map(post => 
-    `
-    <div class="post">
-      <div class="post__title">
-        ${post.Title}
-      </div>
-      <p class="post__body">
-        ${post.body}
-      </p>
-    </div>`).join('');
+  renderPosts(id)
 }
 
-async function renderPosts() {
+async function renderPosts(id) {
    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
    const postsData = await posts.json()
-postListEl.innerHTML = postsData.map(post => .join('')); 
-}
-
+postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
+}   
 function postHTML(post) {
-     `
+return `
     <div class="post">
       <div class="post__title">
         ${post.Title}
@@ -36,4 +23,5 @@ function postHTML(post) {
     </div>
     `
 }
-renderPosts();
+
+renderPosts(id);
