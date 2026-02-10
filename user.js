53f034192bbcc5fed1c1de0acdc03773b2c29501
curt vance <c.vance7777@hotmail.com>
2026-02-10 17:51:14 -0500

    update

--- user.js
+++ user.js
@@ -8,7 +8,7 @@ async function onSearchChange(event) {
 }
 
 async function renderPosts(id) {
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${id}`);
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
     const postsData = await posts.json()
     postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
 
