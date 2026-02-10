f009e885b00b19efdd2845f80c631a9352835901
curt vance <c.vance7777@hotmail.com>
2026-02-10 13:19:03 -0500

    update

--- user.js
+++ user.js
@@ -1,11 +1,13 @@
 const postListEl = document.querySelector('.post-list');
-async function main() {
-   const id = localStorage.getItem("id")
-   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-   const postsData = await posts.json()
-console.log(postsData)
+const event = localStorage.getItem("id")
 
-postListEl.innerHTML = postsData.map(post => `
+async function onSearchChange(event) {
+  const id = event.target.value
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+  const postsData = await posts.json()
+console.log(postsData)
+postListEl.innerHTML = postsData.map(post => 
+    `
     <div class="post">
       <div class="post__title">
         ${post.Title}
@@ -15,4 +17,23 @@ postListEl.innerHTML = postsData.map(post => `
       </p>
     </div>`).join('');
 }
-main();
+
+async function renderPosts() {
+   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+   const postsData = await posts.json()
+postListEl.innerHTML = postsData.map(post => ).join(''); 
+}
+
+function postHTML(post) {
+     `
+    <div class="post">
+      <div class="post__title">
+        ${post.Title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>
+    `
+}
+renderPosts();
