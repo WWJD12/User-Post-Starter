76ab8edfc8dd6c8da74b643b84a633dd2abcf7fb
curt vance <c.vance7777@hotmail.com>
2026-02-11 09:22:22 -0500

    update

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-const  userListEl = document.querySelector('.user-list');
+const userListEl = document.querySelector('.user-list');
 
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
-    const usersData = await users.json();
+    const usersData = await users.json()
     console.log(usersData);
-    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+   userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("") 
 }
-
-
 main()
 
 function showUserPost(id) {
     localStorage.setItem("id", id);
     window.location.href = `${window.location.origin}/user.html`
 }
+
 function userHTML(user) {
-    return `<div class="user-card" onclick="showUserPost(${user.id})">
+return `<div class="user-card" onclick="showUserPost(${user.id})">
     <div class="user-card__container">
-    <h3>${user.name}</h3>
-      <p><b>Email:</b> ${user.email}</p>
-      <p><b>Phone:</b> ${user.phone}</p>
-      <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
-      ${user.website}
-      </a></p>
-      </div>
-      </div>`
-    }
+        <h3>${user.name}</h4>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <p><b>Website:</b> <a href="https://website.website" target="_blank">
+        ${user.website}
+        </a></p>
+    </div>
+    </div>`
+}
