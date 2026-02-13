fbdf65590be3fe51335c533c69c76d0ecd665069
curt vance <c.vance7777@hotmail.com>
2026-02-13 10:23:25 -0500

    url

--- index.js
+++ index.js
@@ -3,27 +3,26 @@
 const userListEl = document.querySelector('.user-list');
 
 async function main() {
-    const users = await fetch("https://jsonplaceholder.typicode.com/users");
-    const usersData = await users.json()
-    console.log(usersData);
-   userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("") 
+const users = await fetch("https://jsonplaceholder.typicode.com/users");
+const usersData = await users.json();   
+userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+    
 }
-main()
-
-function showUserPost(id) {
+main();
+function showUserPosts(id) {
     localStorage.setItem("id", id);
     window.location.href = `${window.location.origin}/user.html`
 }
 
 function userHTML(user) {
-return `<div class="user-card" onclick="showUserPost(${user.id})">
+  return    `<div class="user-card" onclick="showUserPosts(${user.id})">
     <div class="user-card__container">
         <h3>${user.name}</h4>
         <p><b>Email:</b> ${user.email}</p>
         <p><b>Phone:</b> ${user.phone}</p>
-        <p><b>Website:</b> <a href="https://website.website" target="_blank">
+        <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
         ${user.website}
         </a></p>
     </div>
-    </div>`
+    </div>`;
 }
